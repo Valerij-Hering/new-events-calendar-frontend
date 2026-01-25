2efadc782bb53ebcf3ccffb09556f0369140acf2
“Valerij-Hering” <zell9113@gmail.com>
2026-01-26 00:03:50 +0100

    merge remote changes

--- src/features/createEvent/CreateEventForm.jsx
+++ src/features/createEvent/CreateEventForm.jsx
@@ -171,7 +171,7 @@ export const CreateEventForm = ({ initialData, onClose }) => {
                     : t("button.Create")
                 }
             </Button>
-            <Button variant='subtle' color='dark' noPadding type="button" onClick={() => { onClose?.(); !onClose && navigate("/"); }}>
+            <Button variant="clear" color='dark' noPadding type="button" onClick={() => { onClose?.(); !onClose && navigate("/"); }}>
                 {t("button.Close")}
             </Button>
             </Stack>
